bedb0c39c068d02f03beeb7fe8a2edef4a0fee9c
Mikolaj Panasiuk <m.panasiuk.14@outlook.com>
2018-03-29 14:52:39 +0100

    showcase generator

--- dissert/main/analyzer/generate_showcase.py
+++ dissert/main/analyzer/generate_showcase.py
@@ -3,23 +3,30 @@ from os.path import isfile, join
 import sys
 import json
 
+# This scripts creates a 'showcase'
+# A showcase is an animation comprised of all clips in a folder
+# The purpose of a showcase is to create a video that shows a clip and corresponding filename
+# This allows to quickly watch and analyze emotions conveyed by each animation clip
+
+# Get all the files from directory provided through a command line argument
 onlyfiles = sorted([f for f in listdir(sys.argv[1]) if isfile(join(sys.argv[1], f))])
 print onlyfiles
 
 conversation = []
 
+# Create a dialogue line for each animation clip
 for f in onlyfiles:
     name = f.split(" ")[0]
     text = {
-        "text": f,
+        "text": f, # The dialogue line is the animation's name
         "character": "AGENT SMITH",
         "clip": [
             {
-                "name": name,
+                "name": name, # select animation file
                 "file": f
             }
         ],
-        "tones": {
+        "tones": { # Tones are irrelevant for generating a showcase but they cannot be null
             "joy": 0.0,
             "sadness": 0.0,
             "disgust": 0.0,
@@ -30,27 +37,9 @@ for f in onlyfiles:
     }
     conversation.append(text)
 
-
-textendtext = {
-    "text": "test",
-    "character": "AGENT SMITH 2",
-    "clip": [
-        {
-            "name": "test",
-            "file": onlyfiles[0]
-        }
-    ],
-    "tones": {
-        "joy": 0.0,
-        "sadness": 0.0,
-        "disgust": 0.0,
-        "anger": 0.0,
-        "surprise": 0.0,
-        "fear": 0.0
-    }
-}
-
 j = 0
+# Split the conversation into many conversations of 10 dialogue lines
+# A few shorter scenes are easier to analyze and render than one long scene
 for i in range(0, len(conversation), 10):
     starti = i
     endi = i+10
@@ -58,6 +47,7 @@ for i in range(0, len(conversation), 10):
         endi = len(conversation)-1
     con = conversation[starti:endi]
 
+    # Save scenes to files
     file = open("showcases/showcase"+str(j)+".json", "w")
     file.write(json.dumps(con, ensure_ascii=False, indent=4))
     file.close()
