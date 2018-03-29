fe7c1d179223f2decb6677d211b2dc810755ba96
Mikolaj Panasiuk <m.panasiuk.14@outlook.com>
2018-03-29 14:48:16 +0100

    comment script analyze

--- dissert/main/analyzer/script_analyze.py
+++ dissert/main/analyzer/script_analyze.py
@@ -5,27 +5,15 @@ import db
 import sys
 import pprint
 
-conversations = [
-        {'text': " I hate this place.  This zoo. This prison.  This reality, whatever you want to call it, I can't stand it any longer.  It's the smell, if there is such a thing.  I feel saturated by it.  I can taste your stink and every time I do, I fear that I've somehow been infected by it.", 
-        'character': 'AGENT SMITH',
-        'tones': [{'score': 0.986412, 'name': u'frustrated'}, {'score': 0.91887, 'name': u'impolite'}, {'score': 0.836783, 'name': u'sad'}]},
-        {'text': " Repulsive, isn't it?", 
-        'character': 'AGENT SMITH', 
-        'tones': []},
-        {'text': ' I must get out of here, I must get free.  In this mind is tlie key. My key.', 
-        'character': 'AGENT SMITH', 
-        'tones': [{'score': 0.613018, 'name': u'sad'}]},
-]
-
-conversations = parse.parsefile(sys.argv[1])
+# This is the main script of the analyzer module
+# Command line arguments - filepath to script to analyze
+# This program takes in a script as input, analyzes emotions, matches the animations
+# This program outputs a json file instructions of how to assemble the animation
 
 
-for conversation in conversations:
-    tones = tone.analyze(conversation["text"])
-
-    conversation["tones"] = tones
-
+# Ensure emotions are compatible for use by the db.py
 def normalize_emotions(conversation):
+    # Move the tones to one dictionary instead of a list of dictionaries
     tones = {}
     for tone in conversation['tones']:
         tones[tone['name']] = tone['score']
@@ -35,47 +23,61 @@ def normalize_emotions(conversation):
     newtones['fear'] = tones['fear']
     newtones['joy'] = tones['joy']
     newtones['disgust'] = tones['disgust']
-    newtones['surprise'] = 0.0
+    newtones['surprise'] = 0.0 # Surprise is not supported by IBM Watson; ensure value is always 0
     newtones['sadness'] = tones['sadness']
     conversation['tones'] = newtones
 
+    # Decrease the value of joy if anger is detected
+    # Anger is a more important conflicting emotion that shadows joy
     if newtones['anger'] > 0.2:
         newtones["joy"] /= 4.0
 
+# Ensure animations do not repeat too often
 def analyze_repeating(conversations):
     previously_used = []
     for conversation in conversations:
-        clipcopy = list(conversation['clip'])
         lastindex = 0
+        # Iterate the list of matching animations
         for clip in conversation['clip']:
-            if clip['name'] not in previously_used:
+            if clip['name'] not in previously_used: # Choose the animation that was not yet used
                 break
             lastindex += 1
 
+        # Choose betweent the most matching and never previously used animation
         newclip = conversation['clip'][lastindex]
-        if newclip['score'] >= 0.2:
+        if newclip['score'] >= 0.2: # If match score is bigger than 0.2, use the never used animation
             conversation['clip'] = [newclip]
-        else: 
+        else: # Use the most matching animation
             conversation['clip'] = [conversation['clip'][0]]
-        previously_used.append(conversation['clip'][0]['name'])
-
-        print("FINAL CHOICE:", conversation["text"], "ANIM:", conversation['clip'][0]['name'])
-        for clip in clipcopy:
-            print clip["name"], ", ",
-        print()
+        previously_used.append(conversation['clip'][0]['name']) # Remember the names of used animations
 
 
-for conversation in conversations:
-    normalize_emotions(conversation)
 
-for conversation in conversations:
-    conversation['clip'] = db.get_matching_gestures(conversation["text"], conversation['tones'])
+def main():
+    # Read script from provided file
+    conversations = parse.parsefile(sys.argv[1])
 
-analyze_repeating(conversations)
+    # For each dialogue line analyze emotions
+    for conversation in conversations:
+        tones = tone.analyze(conversation["text"]) # use tone.py to analyze emotions
+        conversation["tones"] = tones
 
+    # Normalize emotions of each dialogue line
+    for conversation in conversations:
+        normalize_emotions(conversation)
 
-file = open("scene.json", "w")
-file.write(json.dumps(conversations, ensure_ascii=False, indent=4))
-file.close()
+    # Find animations matching each dialogue line
+    for conversation in conversations:
+        conversation['clip'] = db.get_matching_gestures(conversation["text"], conversation['tones'])
 
-print("output: scene.json")
+    # Ensure animations do not repeat too often
+    analyze_repeating(conversations)
+
+    # Save the instructions to JSON
+    file = open("scene.json", "w")
+    file.write(json.dumps(conversations, ensure_ascii=False, indent=4))
+    file.close()
+
+    print("output: scene.json")
+
+main()
